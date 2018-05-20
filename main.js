cfe905b43a7d6ec6f258d749d7bcae852e0e24ba
Magnus <38915578+magnusriga@users.noreply.github.com>
2018-05-21 00:15:57 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,34 @@
+// namespaced
+// ensures animation only runs every few seconds
+(function() {
+  document.addEventListener('DOMContentLoaded', () => {
+    
+    function startShaking(id, animName, duration, delay) {
+      let timer;
+      const shakingBtn = document.querySelector(id);
+      const animDur = duration/1000 + "s";
+      
+      function addClass() {
+        shakingBtn.classList.add(animName);
+        shakingBtn.style['animation-duration'] = animDur;
+      }
+
+      function removeClass() {
+        shakingBtn.classList.remove(animName);
+      }
+
+      setInterval(() => {
+        // in case duration is longer than delay
+        // we don't want it to suddenly stop in
+        // the middle of next duration.
+        // Thus, clear previous setTimeout() on each start
+        clearTimeout(timer);
+        addClass();
+        timer = setTimeout(() => removeClass(), duration);
+      }, duration + delay);
+    }
+
+    startShaking('#shake-btn', 'anim', 1000, 3000);
+    
+  });
+})();
